5e0766c7cc4a538cdb4dbefe6346f3638cb57fee
mgmaresca <mariagarciamaresca@gmail.com>
2015-05-18 19:10:11 +0200

    Added create new question

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,9 @@ router.param('quizId', quizController.load);  // autoload :quizId
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+
 router.get('/quizes?search=(*)', quizController.index);
 
 module.exports = router;
