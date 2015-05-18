ac13c7915dcea2882f17279a09f27be8b5e6db2e
oscarandresgonzalez <mariagarciamaresca@gmail.com>
2015-05-18 22:20:06 +0200

    Borrar pregunta

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.post('/quizes/create',              quizController.create);
 router.get('/quizes?search=(*)', quizController.index);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 
 module.exports = router;
